3de694b595e50f5c98e6cbf5d6cec928a0c4b07b
Hakan Altındağ <hakangoudberg@hotmail.com>
2025-01-23 10:18:18 +0100

    Update senzu.rb

--- Formula/senzu.rb
+++ Formula/senzu.rb
@@ -3,10 +3,10 @@
 class Senzu < Formula
   desc "CLI tool to get the battery percentage"
   homepage "https://github.com/Hakky54/senzu"
-  url "https://github.com/Hakky54/senzu/releases/download/1.0.0/senzu-macos-amd64.tar.gz"
-  sha256 "bf1e411411a5e5db6a16f05b38699401c9fb9cac4bde2597666ea46bec4e3eb6"
+  url "https://github.com/Hakky54/senzu/releases/download/1.0.1/senzu-macos-amd64.tar.gz"
+  sha256 "583165841e23dbbbafaa1d20dd38ba3dcade16ade20f60f1be7a0304af629557"
   license "Apache-2.0"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "senzu"
